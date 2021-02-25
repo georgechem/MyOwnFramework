10e05c5a051a43a86547fde917bf273c19e8d118
root <grzegorzchrymecki@php-developer.ie>
2021-02-25 18:36:49 +0000

    all ok

--- App/Http/Route/Route.php
+++ App/Http/Route/Route.php
@@ -6,5 +6,49 @@ namespace App\Http\Route;
 
 class Route
 {
+    private static string $controller;
+
+    private static string $method;
+
+    private static array $params;
+
+    private static array $uri;
+
+    public static function splitUri()
+    {
+        $uriExplode = explode('/',$_SERVER['REQUEST_URI'], 4);
+
+
+        if (isset($uriExplode[1]) && !empty($uriExplode[1])) {
+            preg_match('/[a-zA-Z-]+/', $uriExplode[1], $tmp);
+            self::$controller = $tmp[0];
+        } else {
+            self::$controller = 'pages';
+        }
+
+
+        if (isset($uriExplode[2]) && !empty($uriExplode[2])) {
+            preg_match('/[a-zA-Z-]+/', $uriExplode[2], $tmp);
+            self::$method = $tmp[0];
+        } else {
+            self::$method = 'index';
+        }
+
+
+        if (isset($uriExplode[3]) && !empty($uriExplode[3])) {
+            preg_match('/[0-9]+/', $uriExplode[3], $tmp);
+            self::$params = $tmp;
+        } else {
+            self::$params = [];
+        }
+
+        return [
+            'controller' => self::$controller,
+            'method' => self::$method,
+            'params'=> self::$params,
+        ];
+
+
+    }
 
 }
--- App/Http/Router/Router.php
+++ App/Http/Router/Router.php
@@ -4,7 +4,21 @@
 namespace App\Http\Router;
 
 
+use App\Http\Route\Route;
+
 class Router
 {
+    private $route;
+
+    public function __construct()
+    {
+        $this->start();
+    }
+
+    private function start()
+    {
+        $this->route = Route::splitUri();
+        print_r($this->route);
+    }
 
 }
