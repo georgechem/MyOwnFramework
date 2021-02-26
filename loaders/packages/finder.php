18b6d4e2788929bc43bb998c674e52a0e1c1fd2f
root <grzegorzchrymecki@php-developer.ie>
2021-02-26 16:57:18 +0000

    finder removed, public/css created

--- loaders/packages/finder.php
+++ loaders/packages/finder.php
@@ -2,7 +2,9 @@
 
 use Symfony\Component\Finder\Finder;
 
+/**
+ * FINDER FOR public location
+ */
 $finder_public = new Finder();
-
-$finder_public->in($_ENV['variable']['root_path']);
+$finder_public->in($_ENV['variable']['root_path'].'/public/');
 
