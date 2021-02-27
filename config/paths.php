4b99c0719937b6d0be1d061fd70f9d15b50b4570
root <grzegorzchrymecki@php-developer.ie>
2021-02-27 15:54:43 +0000

    behat ready to go

--- config/paths.php
+++ config/paths.php
@@ -3,5 +3,4 @@
 return [
     'root' => dirname(__DIR__). '/',
     'public' => dirname(__DIR__). '/public/',
-    'public2' => getcwd()."/",
 ];
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,6 +1,7 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
@@ -18,5 +19,37 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given there is a :arg1, which costs £:arg2
+     */
+    public function thereIsAWhichCostsPs($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add the :arg1 to the basket
+     */
+    public function iAddTheToTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 product in the basket
+     */
+    public function iShouldHaveProductInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the overall basket price should be £:arg1
+     */
+    public function theOverallBasketPriceShouldBePs($arg1)
+    {
+        throw new PendingException();
     }
 }
